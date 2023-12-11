789da0760adde916f9fa8660292298f556ec5faa
Valkon <96625039+Nawija@users.noreply.github.com>
2023-12-11 17:49:19 +0100

    sdfsdf

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,26 @@
 "use client";
 
+import { useRef } from "react";
+import Autoplay from "embla-carousel-autoplay";
 import { Carousel } from "@mantine/carousel";
 import "@mantine/carousel/styles.css";
 
 function Component() {
+    const autoplay = useRef(Autoplay({ delay: 2000 }));
     return (
         <div className="max-w-5xl mx-auto w-full text-center flex items-center justify-center">
-            <Carousel withIndicators height={800}>
+            <Carousel
+                withIndicators
+                height={200}
+                plugins={[autoplay.current]}
+                onMouseEnter={autoplay.current.stop}
+                onMouseLeave={autoplay.current.reset}
+            >
                 <Carousel.Slide>
-                    <img src="https://cdn.pixabay.com/photo/2023/04/21/17/47/plum-blossoms-7942343_1280.jpg" />
+                    <img
+                        src="https://cdn.pixabay.com/photo/2023/04/21/17/47/plum-blossoms-7942343_1280.jpg"
+                        className="w-full h-full mx-auto text-center"
+                    />
                 </Carousel.Slide>
                 <Carousel.Slide>
                     <img src="https://cdn.pixabay.com/photo/2023/04/21/17/47/plum-blossoms-7942343_1280.jpg" />
