78c0d1aa561acb8afeaae0726081889c60d124c6
Valkon <96625039+Nawija@users.noreply.github.com>
2023-12-13 15:38:09 +0100

    df

--- src/ui/buttons/SecondButton.tsx
+++ src/ui/buttons/SecondButton.tsx
@@ -1,19 +1,20 @@
+import React from "react";
 import clsx from "clsx";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export function SecondButton({ children, className, ...rest }: ButtonProps) {
-    return (
-        <button
-            {...rest}
-            className={clsx(
-                "relative flex items-center border hover:border-white justify-center rounded-lg bg-gray-100 hover:bg-black/10 font-semibold text-xs text-black hover:text-white focus-visible:outline active:bg-white/20 px-3 py-1.5 transition-colors",
-                className
-            )}
-        >
-            {children}
-        </button>
-    );
+  return (
+    <button
+      {...rest}
+      className={clsx(
+        "relative flex items-center justify-center rounded-lg border bg-gray-100 px-3 py-1.5 text-xs font-semibold text-black transition-colors hover:border-white hover:bg-black/10 hover:text-white focus-visible:outline active:bg-white/20",
+        className,
+      )}
+    >
+      {children}
+    </button>
+  );
 }
