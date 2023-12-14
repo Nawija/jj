20287a863c16c270372a5895531e8e81a4f9fcac
Valkon <96625039+Nawija@users.noreply.github.com>
2023-12-14 16:23:27 +0100

    dfggfd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,15 @@
-"use client";
-
 import CarouselPolaroid from "@/src/ui/hero/CarouselPolaroid";
 import { EffectFadeCarousel } from "@/src/ui/hero/EffectFade";
 
 export default function Home() {
   return (
     <div className="anim-opacity mx-auto max-w-[2000px]">
-      <section className="relative w-full -z-10">
+      <section className="relative -z-10 w-full">
         <div className="absolute left-1/2 top-[74%] z-10 w-3/4 -translate-x-1/2 -translate-y-1/2 text-center text-white">
-          <p className=" mb-6 text-3xl font-semibold lg:text-8xl text-white/70 mix-blend-difference">
+          <p className=" mb-6 text-3xl font-semibold text-white/70 mix-blend-difference lg:text-8xl">
             PRAWDZIWA FOTOGRAFIA
           </p>
-          <p className="mb-12 text-sm text-white/80 mx-auto sm:w-3/4">
+          <p className="mx-auto mb-12 text-sm text-white/80 sm:w-3/4">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt
             maiores at quis illum facilis magnam dolorum illo eius quidem autem,
             doloribus harum accusamus. Sed, illo similique. Aliquam, commodi.
